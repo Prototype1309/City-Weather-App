714c027f16529f3e8f84ff13ad9207580a00a5de
prototype1309 <simo.hourri@gmail.com>
2023-04-26 16:18:45 -0400

    Adding quety selectors in JS

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var searchHistoryList = $('#search-history-list');
+var searchCityInput = $("#search-city");
+var searchCityButton = $("#search-city-button");
+var clearHistoryButton = $("#clear-history");
+
+var currentCity = $("#current-city");
+var currentTemp = $("#current-temp");
+var currentHumidity = $("#current-humidity");
+var currentWindSpeed = $("#current-wind-speed");
+var UVindex = $("#uv-index");
+
+var weatherContent = $("#weather-content");
